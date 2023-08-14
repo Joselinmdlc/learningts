157c55c2ca4266910747cc4ccc080ad610f81543
Joselin Melchor <joselin_melchor@hotmail.com>
2023-08-14 16:10:58 -0600

    [ADD] file sintaxs

---
+++ EstructurasCiclicas/Sintax.ts
@@ -0,0 +1,11 @@
+//Sixtaxis For
+
+/* palabra reservada ::for:: ( inicializador ; condicion de termino ; incremeto ) {
+  // cuerpo de ciclo for
+  // la ejecucion a repetir
+}
+*/
+
+for ( let i = 0; i > 100; i = i + 1 ) {
+  //cuerpo del for
+}
