507d53754e5f90f1d1f8c56554d33c1e9ef1d7fd
Joselin Melchor <joselin_melchor@hotmail.com>
2023-08-14 17:02:55 -0600

    [ADD] primer programa con for

---
+++ Ejercicios/EstructurasControl/for-01.ts
@@ -0,0 +1,23 @@
+
+for (let i = 0; i<100; i=i+2){
+
+if (i%2) {
+console.log("El numero es par");}
+else {
+    console.log ("No es par");
+}
+    console.log(i);
+}
+ //programa que corra del 100 al mil
+ // dividir por la unidad el 100 entre 1
+ //crear una variable que vaya incrementando que llegue a 10 se resetee a 0 
+ // cada 10 conteos 
+ //cuando llegue al 110 va a valer 0
+ // cuando llegue a 200 va a valer 10 
+ // cuando legue al 300 va valer 21 22 23
+
+//si el contadpr de for es par se imprime
+ // coincidir si el contador externo tambein es par
+ // si ninguna es par no imprimir
+
+ //pista una variable se inicia afuera
