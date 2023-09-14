4a6355cde4bf425b079da2f6077a6d66ea26f493
Joselin Melchor <joselin_melchor@hotmail.com>
2023-09-14 01:19:09 -0600

    [Udate]Primer ejercicio

--- Ejercicios/EstructurasControl/3 ejercicios/primer.ts
+++ Ejercicios/EstructurasControl/3 ejercicios/primer.ts
@@ -1,27 +1,4 @@
 //Comprobacion de Palindomos.
-/*function textPalindrome(text: string) {
-    let a = text.split("");
-    let reverse = a.reverse();
-    return text == reverse.join("") ? "Si es" : "No es"
-}
-console.log(textPalindrome("amor akalsm roma")) */
-
-
-
-
-/*function isPalindrome(text: string) {
-    text = text.replace(" ", "");
-    text = text.toLowerCase();
-    let i = 0;
-    for (let i = 0; i < text.length / 2; i++) {
-    } if (text.charAt(i) == text.charAt(text.length - 1 - i)){
-        console.log("El texto es Palindromo")
-    }
-    else {
-        console.log("El texto no es Palindromo")
-    }
-}
-isPalindrome("Anita lava joselin la tina")*/
 
 function isPalindrome(text: string) {
     text = text.replace(/\s/g, "").toLowerCase();
